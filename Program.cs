a441b482a48cd6e7b7e454f62667b3a4c4d281b0
avaid <avaid@VAIDYS>
2024-05-09 03:32:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿//using System.Reflection;
+using System.Threading;
+namespace AsyncFileLogger
+{
+    internal class Program
+    {
+        //declare global variables
+        private static readonly object fileLock = new object(); // Lock object for file access
+        private static string filePath = "";
+        private static string fileName = "";
+        private static int lineCount = 0;
+        static void Main(string[] args)
+        {
+            ///check for user argument for filepath
+            //if (args.Length != 1)
+            //{
+            //    Console.WriteLine("Please provide a file path as an argument.");
+            //    return;
+            //}
+            ///assign file path
+            //filePath = args[0];
+            //filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location.ToString());
+
+            //filePath = $"log";
+            ///create file path/overwrite if it exists
+            filePath = "/app/log";
+            if (!Directory.Exists(filePath))
+            {
+                System.IO.Directory.CreateDirectory(filePath);
+            }
+            Console.WriteLine($"File created successfully: {filePath}");
+            fileName = $"{filePath}/out.txt";
+            //fileName = $"/data/out.txt";
+          
+            //initialize the file.  Overwrite existing file.
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                string currentTimeStamp = DateTime.Now.ToString("HH:mm:ss.fff");
+                sw.WriteLine($"Line Number:0,Thread ID:0,Time:{currentTimeStamp}");                
+            }
+
+            ///create 10 threads
+            Thread[] threads = new Thread[10];
+            
+            try
+            {
+                ///start 10 threads
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    threads[i] = new Thread(() => AppendLine(i));
+                    threads[i].Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());                
+            }
+            // Wait for all threads to finish
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("Finished writing to file.");
+        }
+        private static void AppendLine(int threadId)
+        {   
+            ///For every thread, append 10 lines to the file
+            for (int i = 0; i < 10; i++)
+            {
+                string currentTimeStamp = DateTime.Now.ToString("HH:mm:ss.fff");
+                //string line = $"line {i + 1} from thread {threadId},currentTimeStamp";
+                string line = $"Thread ID:{threadId},Time:{currentTimeStamp}";
+                AppendLineToFile(line);
+            }
+                      
+        }
+        private static void AppendLineToFile(string line)
+        {
+            ///lock before writing
+            lock (fileLock) 
+            {
+                lineCount++;///generate the serial number
+                using (StreamWriter writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine($"Line Number:{lineCount},{line}");
+                }
+            }
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+﻿1. This project is developed using VS 2022 with .Net 8.0 framework
+2. Created a console application
+3. You can right click at the project file and create a 'docker support' file
+4. You should have Docker Desktop installed in your local machine
+5. Create a user account in "https://hub.docker.com/"
+5. Create a repository in "https://hub.docker.com/"
+6. Debug your application locally to check if it works.
+7. Publish it to docker.  You need your username and password to the registry.
+8. To test, user can pull the docker container and run it using docker desktop
+9. Use command prompt
